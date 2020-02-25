c23813f3a86c295a9aff1c070e118ddfcffebdc3
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-25 09:02:58 -1000

    object serializer

--- app/src/main/java/com/myappcompany/russell/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/sharedpreferences/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,11 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.myappcompany.russell.sharedpreferences", Context.MODE_PRIVATE);
 
+        ArrayList<String> friends = new ArrayList<>();
+
+        friends.add("fido");
+        friends.add("sara");
+        friends.add("jones");
+
+        try {
+            sharedPreferences.edit().putString("friends", ObjectSerializer.serialize(friends)).apply();
+            Log.i("friends", ObjectSerializer.serialize(friends));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> newFriends = new ArrayList<>();
+
+        try {
+            newFriends = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("friends", ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.i("new Friends", newFriends.toString());
 //        sharedPreferences.edit().putString("username", "russell").apply();
-
-        String username = sharedPreferences.getString("username", "");
-
-        Log.i("This is the username", username);
+//
+//        String username = sharedPreferences.getString("username", "");
+//
+//        Log.i("This is the username", username);
 
 
     }
